8e3a7725b3219243d4ee004c559018d839f56be9
Athar Noor <atharnoor2@gmail.com>
2017-09-20 12:43:49 +0500

    WeatherReport: added validation for country_code and city

--- app/models/weather_report.rb
+++ app/models/weather_report.rb
@@ -4,4 +4,6 @@ class WeatherReport
 
   attr_accessor :country_code, :city
 
+  validates :country_code, presence: true
+  validates :city, presence: true
 end
--- spec/models/weather_report_spec.rb
+++ spec/models/weather_report_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe WeatherReport, type: :model do
   it "is valid with valid attributes" do
     expect(WeatherReport.new(country_code: "US", city: "Andora")).to be_valid
   end
+
+  it "is invalid if country_code is empty" do
+    expect(WeatherReport.new(city: "Andora")).not_to be_valid
+  end
+
+  it "is invalid if city is empty" do
+    expect(WeatherReport.new(country_code: "US")).not_to be_valid
+  end
 end
